bee56453158d68aeafeb13616832cc3c0adc53cb
JorgeACS <jorge_nintendo@hotmail.com>
2015-03-03 21:51:58 -0700

    a

---
+++ Plataforma.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WindowsGame1
+{
+
+    class Plataforma
+    {
+        bool peligro;
+        Rectangle plataforma;
+
+        Plataforma(Rectangle plataforma, bool peligro = false) {
+            this.peligro = peligro;
+            this.plataforma = plataforma;
+        }
+    }
+
+   
+}
